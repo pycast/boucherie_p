3a3a3a841c1af19c4071b1d25f62818f6547048e
SophieCha <sophieadou@hotmail.fr>
2022-03-01 09:33:51 +0100

    liste ingrédients premier essai

--- src/Controller/Admin/TestCrudController.php
+++ src/Controller/Admin/TestCrudController.php
@@ -3,19 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Ingredient;
+use App\Entity\OrderDetails;
 use App\Entity\Quantity;
 use App\Entity\Recipe;
-use App\Form\IngredientType;
-use App\Form\QuantityType;
-use App\Form\RecipeType;
-use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class TestCrudController extends AbstractController
 
@@ -28,118 +24,80 @@ class TestCrudController extends AbstractController
     }
 
      #[Route('/testCrudController', name: 'test_crud_controller')]
-    public function index(Request $request):Response
-    {
-        $notificationRecipe=null;
-        $notificationIngredient=null;
-        $notificationQuantity=null;
-        //First recipe form to insert recipe in database
-        $recipe = new Recipe();
-        $formRecipe = $this->createForm(RecipeType::class, $recipe);
-        $formRecipe->handleRequest($request);
-
-            if ($formRecipe->isSubmitted() && $formRecipe->isValid()){
-                $recipe=$formRecipe->getData();
-                $search_recipe = $this->entityManager->getRepository(Recipe::class)->findOneByProduct($recipe->getProduct());
-                
-                        if (!$search_recipe) {
-                                //if recipe doesn't exist in database:
-                                $this->entityManager->persist($recipe);
-                                $this->entityManager->flush();
-                                $notificationRecipe='Recette ajoutée dans la bdd, maintenant ajoutez des ingrédients';
-                    
-                            }else{
-                                //else if recipe already exists
-                                $notificationRecipe='Cette recette existe déjà, vous pouvez la modifier';    
-                            }
-            }
-
-
-        //Second ingredient form to create ingrédients
-        /**
-         * Etape 1:créer une base d'ingrédients
-         * Etape 2:Obtenir une liste d'ingrédients
-         */
-        $ingredient = new Ingredient();
-        $formIngredient = $this->createForm(IngredientType::class, $ingredient);
-        $formIngredient->handleRequest($request);
-        
-         if ($formIngredient->isSubmitted() && $formIngredient->isValid())
-         {
-                $ingredient=$formIngredient->getData();
-                //dd($ingredient);
-                $this->entityManager->persist($ingredient);
-                $this->entityManager->flush();
-                $notificationIngredient='ingrédient ajouté avec succès';
-            }
-
-        //Third set quantity, link to ingredient, link to recipe 
-       
-        $quantity = new Quantity();
-        $formQuantity = $this->createForm(QuantityType::class, $quantity);
-        $formQuantity->handleRequest($request);
-        
-         if ($formQuantity->isSubmitted() && $formQuantity->isValid())
-         {
-                $quantity=$formQuantity->getData();
-                //dd($ingredient);
-                $this->entityManager->persist($quantity);
-                $this->entityManager->flush();
-                $notificationQuantity='ingrédient ajouté avec succès';
-            }
-
-
-
-        //get recipe ID + Name of product
+    public function index():Response
+    {  
+        //----get recipe ID + Name of product-------
          $repositoryRecipe= $this->entityManager->getRepository(Recipe::class)->findAll();
-
-         $idRecipe=[];
-         foreach ($repositoryRecipe as $recipe){
+         
+         foreach ($repositoryRecipe as $recipeQuantity){
             
-             $idRecipe[]=[
-                 'id'=>$recipe->getId(),'recette'=>$recipe->getProduct()];  
          }
+
+         $idRecipe=[];//ensemble des ID recettes
+         $nameRecipe=[];//ensemble des noms de recettes (=nom du produit)
+         foreach ($repositoryRecipe as $recipe){
+                $products=$recipe->getProduct();
+                 $nameRecipe[]=$products->getName();
+                $idRecipe[]=[
+                    'id'=>$recipe->getId(),
+                    'recette'=>$recipe->getProduct()
+                ];  
+                
+               
+            }
          
-         //get all quantity table
+         //------get all quantity table-----------
           $repositoryQuantities= $this->entityManager->getRepository(Quantity::class)->findAll();
-         
-          //get order table
-         
+        
+          //--------get Quantity of each product ordered---------
           
-             
          
-             
-        
-        
-         //dd($repositoryRecipe);
-        //get ingredients
+             $arrayPrdQut=[];//tableau qui comprends un tableau par recette commandée
+          foreach($nameRecipe as $produitCommandé){
+                //dans le repositoryOrderDetails, je cherche un produit qui corresponde à un nom de recette.
+                //si je trouve c'est que le produit a été commandé au moins une fois ou plus.
+                $repositoryOrderDetails= $this->entityManager->getRepository(OrderDetails::class)->findByProduct($produitCommandé);
+                //si produit trouvé/commandé:
+                if ($repositoryOrderDetails){
+                    
+                    $qt=0;//regroupe ensemble des quantités d'un produit donné.
+                    //à chaque fois que le produit est trouvé, getQuantity +1
+                    foreach($repositoryOrderDetails as $detail){
+                        $qt+=$detail->getQuantity();  
+                    }
+                    //on regroupe le tout dans un tableau, un nom de produit, le nombre de fois où il a été commandé
+                    $arrayPrdQut[]=[
+                        'produit'=>$produitCommandé,
+                        'qté'=>$qt
+                    ];  
+                }
+                
+
+            //-----get ingredient repository---
+            $repositoryIngredient= $this->entityManager->getRepository(Ingredient::class)->findAll();
+                //créer un tableau avec les id des aliments existants
+                $arrayIdIngredient=[];
+                foreach ($repositoryIngredient as $ingredientID){
+                    $arrayIdIngredient[]=$ingredientID->getId();
+                }
+                
+
+          }
+          
          
          
-         
-         
-
-
-
-        
-
-
-
-        
-
-    
-
         
         return $this->render('admin/index.html.twig',[
-            'formRecipe'=>$formRecipe->createView(),
-            'formIngredient'=>$formIngredient->createView(), 
-            'formQuantity'=>$formQuantity->createView(), 
-            'notificationRecipe'=>$notificationRecipe,
-            'notificationIngredient'=>$notificationIngredient,
-            'notificationQuantity'=>$notificationQuantity,
+           
+           
             'repositoryRecipe'=>$repositoryRecipe,
             'repositoryQuantities'=>$repositoryQuantities,
-            'idRecipe'=>$idRecipe
-
+            'idRecipe'=>$idRecipe,//produit(objet) + id de la recette
+            'repositoryOrderDetails'=>$repositoryOrderDetails,
+            'nameRecipe'=>$nameRecipe,//le nom des recettes existantes( correspondent au même nom de produit)
+            'produitCommandé'=>$produitCommandé,//Dans les produits existants, nom des produits commandés (attention pas de tableau ici)
+            'arrayPrdQut'=>$arrayPrdQut,//Array: nom du produit, le nombre de fois où il a été commandé(sur total des commandes).
+            'arrayIdIngredient'=>$arrayIdIngredient
             
 
         ]);
